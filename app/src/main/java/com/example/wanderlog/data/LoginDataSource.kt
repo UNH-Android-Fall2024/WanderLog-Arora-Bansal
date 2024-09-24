3bb2c49573548e2596511122291cdbc0af1306ec
Tarasha Bansal <tarashabansalwork@gmail.com>
2024-09-24 18:58:31 -0400

    Added Login activity with basic working

---
+++ app/src/main/java/com/example/wanderlog/data/LoginDataSource.kt
@@ -0,0 +1,27 @@
+package com.example.wanderlog.data
+
+import android.content.Intent
+import androidx.core.content.ContextCompat.startActivity
+import com.example.wanderlog.MainActivity
+import com.example.wanderlog.data.model.LoggedInUser
+import java.io.IOException
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+class LoginDataSource {
+
+    fun login(username: String, password: String): Result<LoggedInUser> {
+        try {
+            // TODO: handle loggedInUser authentication
+            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), username)
+            return Result.Success(fakeUser)
+        } catch (e: Throwable) {
+            return Result.Error(IOException("Error logging in", e))
+        }
+    }
+
+    fun logout() {
+        // TODO: revoke authentication
+    }
+}
