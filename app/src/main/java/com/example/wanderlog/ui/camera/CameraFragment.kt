df88a7aa250443f548d6ec9f7760e89e60aa6d3c
Tanushree Arora <aroratanushree.28@gmail.com>
2024-10-10 14:53:19 -0400

    added functionality for rotate camera
    not working yet, committed to save the code

--- app/src/main/java/com/example/wanderlog/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/example/wanderlog/ui/camera/CameraFragment.kt
@@ -10,7 +10,9 @@ import com.example.wanderlog.databinding.FragmentCameraBinding
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import android.Manifest
+import android.app.Activity
 import android.content.ContentValues
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.camera.core.ImageCapture
@@ -23,6 +25,7 @@ import android.util.Log
 import java.text.SimpleDateFormat
 import java.util.Locale
 import android.provider.MediaStore
+import androidx.activity.result.ActivityResultLauncher
 import androidx.camera.core.ImageCaptureException
 
 
@@ -36,6 +39,12 @@ class CameraFragment : Fragment() {
 
     private lateinit var cameraExecutor: ExecutorService
 
+    private lateinit var galleryLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+
+    private var cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -47,14 +56,41 @@ class CameraFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // If the permission granted, open the gallery
+                //openGallery()
+            } else {
+                // Permission denied
+                Toast.makeText(requireContext(), "Gallery permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // Initialize the gallery launcher
+        galleryLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK && result.data != null) {
+                val selectedImageUri = result.data?.data
+                // Handle the selected image here
+                Toast.makeText(requireContext(), "Selected Image: $selectedImageUri", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         // Request camera permissions
         if (allPermissionsGranted()) {
             startCamera()
         } else {
             requestPermissions()
         }
-        // Set up the listeners for take photo and video capture buttons
+        // Set up the listeners for take photo and other operations
         viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
+
+        viewBinding.imagePickerButton.setOnClickListener {}
+
+        viewBinding.rotateCameraButton.setOnClickListener{ rotateCamera()}
 
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
@@ -66,10 +102,10 @@ class CameraFragment : Fragment() {
             // Handle Permission granted/rejected
             var permissionGranted = true
             permissions.entries.forEach {
-                if (it.key in REQUIRED_PERMISSIONS && it.value == false)
+                if (it.key in REQUIRED_PERMISSIONS && !it.value)
                     permissionGranted = false
             }
-            if (!permissionGranted) {
+            if (permissionGranted) {
                 Toast.makeText(requireContext(),
                     "Permission request denied",
                     Toast.LENGTH_SHORT).show()
@@ -77,6 +113,34 @@ class CameraFragment : Fragment() {
                 startCamera()
             }
         }
+
+    private fun rotateCamera(){
+        cameraSelector = if (cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA)
+            CameraSelector.DEFAULT_FRONT_CAMERA
+        else
+            CameraSelector.DEFAULT_BACK_CAMERA
+    }
+
+//    // check if gallery permission is granted
+//    private fun checkGalleryPermission() {
+//        if (ContextCompat.checkSelfPermission(
+//                requireContext(),
+//                Manifest.permission.READ_EXTERNAL_STORAGE
+//            ) == PackageManager.PERMISSION_GRANTED
+//        ) {
+//            openGallery()
+//        } else {
+//            // Use the permission launcher to request the permission
+//            permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+//        }
+//    }
+
+    // function for Gallery
+//    private fun openGallery(){
+//        val intent = Intent(Intent.ACTION_PICK)
+//        intent.type = "image/*"
+//        galleryLauncher.launch(intent)
+//    }
 
     private fun takePhoto() {
         // Get a stable reference of the modifiable image capture use case
@@ -137,7 +201,7 @@ class CameraFragment : Fragment() {
                 .build()
 
             // Select back camera as a default
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
             try {
                 // Unbind use cases before rebinding
@@ -173,7 +237,8 @@ class CameraFragment : Fragment() {
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private val REQUIRED_PERMISSIONS =
             mutableListOf (
-                Manifest.permission.CAMERA
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_EXTERNAL_STORAGE // for gallery
             ).apply {
                 if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
                     add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
