0fe82a427c36b559d046b0943b0c59faff2899df
Tanushree Arora <aroratanushree.28@gmail.com>
2024-10-24 16:33:46 -0400

    image saving location updated

--- app/src/main/java/com/example/wanderlog/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/example/wanderlog/ui/camera/CameraFragment.kt
@@ -169,7 +169,7 @@ class CameraFragment : Fragment() {
             put(MediaStore.MediaColumns.DISPLAY_NAME, name)
             put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
             if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/")
             }
         }
 
