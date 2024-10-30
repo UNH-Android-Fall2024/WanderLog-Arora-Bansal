d6d0a2892996f3834fd8f2dc2fc0a8214fe5604a
Tarasha Bansal <tarashabansalwork@gmail.com>
2024-10-30 19:21:28 -0400

    Minor bug fixes

--- app/src/main/java/com/example/wanderlog/dataModel/Post.kt
+++ app/src/main/java/com/example/wanderlog/dataModel/Post.kt
@@ -6,7 +6,7 @@ class Post (
     val userID: String = "",
     val content: String = "",
     val imageUrl: String = "",
-    val comments: ArrayList<List<String>> = ArrayList(),
+    val comments: ArrayList<HashMap<String,String>> = ArrayList(),
     val likes: ArrayList<String> = ArrayList(),
     val location: ArrayList<String> = ArrayList(),
     )
--- app/src/main/java/com/example/wanderlog/ui/camera/AddPostFragment.kt
+++ app/src/main/java/com/example/wanderlog/ui/camera/AddPostFragment.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,22 +15,18 @@ import androidx.navigation.fragment.findNavController
 import com.example.wanderlog.R
 import com.example.wanderlog.dataModel.Post
 import com.example.wanderlog.databinding.FragmentAddPostBinding
-import com.example.wanderlog.ui.home.HomeFragment
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
-import com.google.android.gms.tasks.Continuation
-import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.firestore
+import com.google.firebase.storage.storage
 
 class AddPostFragment : Fragment() {
     private lateinit var viewBinding: FragmentAddPostBinding
     private var imageUri: Uri? = null
-
+    private var auth = Firebase.auth
     private val db = Firebase.firestore
+    private var storage = Firebase.storage
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -70,48 +67,66 @@ class AddPostFragment : Fragment() {
             Toast.makeText(requireContext(), "Please write a caption.", Toast.LENGTH_LONG).show()
             return
         }
-        var storagePostPictureRef = FirebaseStorage.getInstance().reference
-        val fileRef = storagePostPictureRef.child(("posts/${FirebaseAuth.getInstance().currentUser!!.uid}/${System.currentTimeMillis()}.jpg"))
-        val uploadTask = fileRef.putFile(imageUri!!)
+        var uniqueID= System.currentTimeMillis()
+        var postPath = "posts/${auth.currentUser!!.uid}/${uniqueID}.jpg"
+        val storageRef = storage.reference
+        val riversRef = storageRef.child(postPath)
+        val uploadTask = riversRef.putFile(imageUri!!)
 
-        val ref = FirebaseDatabase.getInstance().reference.child("posts")
-        val postId = ref.push().key!!
-        val path = "posts/${FirebaseAuth.getInstance()}/${postId}.jpg"
-
-        uploadTask.continueWithTask { task ->
-            if (!task.isSuccessful) {
-                task.exception?.let { throw it }
-            }
-            fileRef.downloadUrl
-        }.addOnCompleteListener { task ->
-            val imageUrl = task.result.toString()
-            val post = Post(FirebaseAuth.getInstance().currentUser!!.uid,
-                viewBinding.captionInput.text.toString(), path
+        uploadTask.addOnFailureListener {
+            Log.d("uploadedfail",postPath)
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
+//            val userID: String = "",
+//    val content: String = "",
+//    val imageUrl: String = "",
+//    val comments: ArrayList<HashMap<String,String>> = ArrayList(),
+//    val likes: ArrayList<String> = ArrayList(),
+//    val location: ArrayList<String> = ArrayList(),
+            val submit = Post(
+                auth.currentUser!!.uid,
+                viewBinding.captionInput.text.toString(),
+                postPath
             )
+            db.collection("posts").document(uniqueID.toString()).set(submit, SetOptions.merge())
+            Log.d("uploaded","Success $postPath")
+            Toast.makeText(requireContext(), "Selected Image: $imageUri", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_addPostFragment_to_navigation_home)
 
-            db.collection("posts").document(postId).set(post, SetOptions.merge())
-                .addOnSuccessListener {
-                    Toast.makeText(requireContext(), "Post uploaded successfully", Toast.LENGTH_LONG).show()
-                    findNavController().navigate(R.id.action_addPostFragment_to_navigation_home)
-
-                }
-                .addOnFailureListener { exception ->
-                    Toast.makeText(requireContext(), "Failed to upload post: ${exception.message}", Toast.LENGTH_SHORT).show()
-                }
-
-                }
-                        .addOnFailureListener { exception ->
-                            Toast.makeText(
-                                requireContext(),
-                                "Failed to upload post: ${exception.message}",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-            .addOnFailureListener {
-            Toast.makeText(requireContext(), "Failed to upload image: ${it.message}", Toast.LENGTH_SHORT).show()
         }
+//        uploadTask.continueWithTask { task ->
+//            if (!task.isSuccessful) {
+//                task.exception?.let { throw it }
+//            }
+//            fileRef.downloadUrl
+//        }.addOnCompleteListener { task ->
+//            val imageUrl = task.result.toString()
+//            val post = Post(auth.currentUser!!.uid,
+//                viewBinding.captionInput.text.toString(), path
+//            )
+//
+//            db.collection("posts").document(postId).set(post, SetOptions.merge())
+//                .addOnSuccessListener {
+//                    Toast.makeText(requireContext(), "Post uploaded successfully", Toast.LENGTH_LONG).show()
+//
+//                }
+//                .addOnFailureListener { exception ->
+//                    Toast.makeText(requireContext(), "Failed to upload post: ${exception.message}", Toast.LENGTH_SHORT).show()
+//                }
+//
+//                }
+//                        .addOnFailureListener { exception ->
+//                            Toast.makeText(
+//                                requireContext(),
+//                                "Failed to upload post: ${exception.message}",
+//                                Toast.LENGTH_SHORT
+//                            ).show()
+//                        }
+//            .addOnFailureListener {
+//            Toast.makeText(requireContext(), "Failed to upload image: ${it.message}", Toast.LENGTH_SHORT).show()
+//        }
     }
-
     companion object {
         fun newInstance(imageUri: Uri): AddPostFragment {
             return AddPostFragment().apply {
